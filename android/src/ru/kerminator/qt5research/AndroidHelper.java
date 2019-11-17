0ea389758c8174dce76e0502f87536cad1724fb5
Maksim Rozhkov <kermit@mail.ru>
2019-11-17 14:43:58 +0300

    Технологический commit

--- android/src/ru/kerminator/qt5research/AndroidHelper.java
+++ android/src/ru/kerminator/qt5research/AndroidHelper.java
@@ -1,11 +1,5 @@
 package ru.kerminator.qt5research;
 
-import android.*;
-import android.Manifest;
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-
 // Класс AndroidHelper выполняет вспомогательную функцию, что следует из
 // его названия. Экземпляр этого класса создаётся в С++ коде registerNativeMethods()
 // с целью связывания native-методов (Java) с их реализацией на C++
